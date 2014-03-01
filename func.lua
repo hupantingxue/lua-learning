c1567da638e55962e6dc9daf6fbbd185c8788312
hupantingxue <hupantingxue@126.com>
2014-03-01 11:21:09 +0800

    Lua function test

---
+++ func.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+-- function test
+
+function create_a_counter()
+    local count = 0
+	return function()
+	    count = count + 1
+		return count
+	end
+end
+
+fun = create_a_counter()
+print(fun)
